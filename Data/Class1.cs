8e60446788005f9e0f9195e9658c7a78c3f120b6
けつ <rrrrkets@gmail.com>
2021-10-22 22:33:09 +0900

    new

---
+++ Data/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class TempData
+    {
+        public DateTime DateTime { get; set; }
+        public double Temp { get; set; }
+    }
+}
